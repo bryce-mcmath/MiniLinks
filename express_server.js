eefba7f50215e10fd5561517405255c2bdb78ab7
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-01-21 15:49:47 -0800

    Comment routes, move helper functions to seperate file. Switch cookie from username to user object

--- express_server.js
+++ express_server.js
@@ -4,115 +4,242 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 5050;
 
+// Helper functions
+const {
+  passwordIsValid,
+  emailIsTaken,
+  generateRandomString
+} = require('./helpers');
+
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const generateRandomString = () => {
-  // assigns an array of random alphanumeric characters (lowercase)
-  let str = Math.random()
-    .toString(36)
-    .substring(2, 8)
-    .split('');
-
-  let alph = 'abcdefghijklmnopqrstuvwxyz';
-
-  for (let i = 0; i < str.length; i++) {
-    // If it's a alphabet char, randomly uppercase
-    if (alph.indexOf(str[i]) > -1 && Math.round(Math.random()) === 1) {
-      str[i] = str[i].toUpperCase();
-    }
-  }
-
-  // returns joined string
-  return str.join('');
-};
-
-let username = '';
+// Objects acting as stand-ins for a database
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+const users = {
+  A7ei0p: {
+    name: 'Andy',
+    id: 'A7ei0p',
+    email: 'andy@gmail.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  lL32o0: {
+    name: 'Mark',
+    id: 'lL32o0',
+    email: 'mark@gmail.com',
+    password: 'dishwasher-funk'
+  },
+  e7I9U0: {
+    name: 'Bryce McMath',
+    id: 'e7I9U0',
+    email: 'bryce.j.mcmath@gmail.com',
+    password: '123456'
+  }
+};
 
+/****************************************
+ *************UTILITY ROUTES*************
+ ***************************************/
+/**
+ * Redirects to urls page if logged in, otherwise
+ * redirects to registration page
+ * @method get
+ */
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  if (req.cookies['user_id']) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/register');
+  }
 });
 
-app.get('/urls/new', (req, res) => {
-  const templateVars = {
-    username: req.cookies.username
-  };
-  res.render('urls_new', templateVars);
+/**
+ * Redirects from shortened URL to assigned long URL. Main functionality
+ * @method get
+ */
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+/*****************************************
+ *********LOGIN/REGISTER ROUTES***********
+ ****************************************/
+/**
+ * Renders register page
+ * @method get
+ */
+app.get('/register', (req, res) => {
+  if (req.cookies['user_id']) {
+    res.redirect('/urls');
+  } else {
+    res.render('register', { user: undefined });
+  }
 });
 
+/**
+ * Handles registration requests
+ * @method post
+ */
+app.post('/register', (req, res) => {
+  // Destructure info passed into post request
+  const { name, email, password, password2 } = req.body;
+
+  // Check the passwords match (should also check on front end)
+  if (
+    password &&
+    name &&
+    email &&
+    password === password2 &&
+    !emailIsTaken(email, users)
+  ) {
+    let id = generateRandomString();
+    while (Object.keys(users).indexOf(id) !== -1) {
+      id = generateRandomString();
+    }
+
+    // Add new user to users object
+    users[id] = {
+      name,
+      email: email.toLowerCase(),
+      id,
+      password
+    };
+
+    res.cookie('user_id', id);
+    res.redirect('/urls');
+  } else {
+    res.status(400);
+    res.redirect('back');
+  }
+});
+
+/**
+ * Renders login page
+ * @method get
+ */
+app.get('/login', (req, res) => {
+  const id = req.cookies['user_id'];
+  if (id) {
+    res.redirect('/urls');
+  } else {
+    res.render('login', { user: undefined });
+  }
+});
+
+/**
+ * Handles login requests
+ * @method post
+ */
+app.post('/login', (req, res) => {
+  // Destructure info passed into post request
+  const { email, password } = req.body;
+
+  // Check the passwords match (should also check on front end)
+  if (password && email && passwordIsValid(email, users, password)) {
+    const id = passwordIsValid(email, users, password);
+    res.cookie('user_id', id);
+    res.redirect('/urls');
+  } else {
+    res.status(403);
+    res.redirect('back');
+  }
+});
+
+/**
+ * Handles logout requests
+ * @method post
+ */
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/register');
+});
+
+/*****************************************
+ ***************URL ROUTES****************
+ ****************************************/
+/**
+ * Renders main page which lists urls
+ * @method get
+ */
 app.get('/urls', (req, res) => {
-  console.log(req.cookies.username);
-  let templateVars = {
-    username: req.cookies.username,
-    urls: urlDatabase
-  };
-  res.render('urls_index', templateVars);
+  if (req.cookies['user_id']) {
+    const templateVars = {
+      user: users[req.cookies['user_id']],
+      urls: urlDatabase
+    };
+    res.render('urls_index', templateVars);
+  } else {
+    res.redirect('/register');
+  }
 });
 
+/**
+ * Handles new short URL requests
+ * @method post
+ */
+app.post('/urls', (req, res) => {
+  let shortURL = generateRandomString();
+  while (Object.keys(urlDatabase).indexOf(shortURL) !== -1) {
+    shortURL = generateRandomString();
+  }
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(`/urls/${shortURL}`);
+});
+
+/**
+ * Renders page for creating new short URLS
+ * @method get
+ */
+app.get('/urls/new', (req, res) => {
+  if (req.cookies['user_id']) {
+    const templateVars = {
+      user: users[req.cookies['user_id']]
+    };
+    res.render('urls_new', templateVars);
+  } else {
+    res.redirect('/register');
+  }
+});
+
+/**
+ * Renders specific page for a given short URL
+ * @method get
+ */
 app.get('/urls/:shortURL', (req, res) => {
-  console.log(req.cookies.username);
-  let templateVars = {
-    username: req.cookies.username,
+  const templateVars = {
+    user: users[req.cookies['user_id']],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render('urls_show', templateVars);
 });
 
-app.post('/urls/:id', (req, res) => {
-  const shortURL = req.params.id;
+/**
+ * Handles requests to update a short URL's corresponding long URL
+ * @method post
+ */
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  let shortURL = generateRandomString();
-  while (Object.keys(urlDatabase).indexOf(shortURL) !== -1) {
-    shortURL = generateRandomString();
-  }
-  urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
-});
-
+/**
+ * Handles requests to delete a short URL
+ * @method post
+ */
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
-  console.log('Updated database: ', urlDatabase);
-  res.redirect('/urls');
-});
-
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-  console.log('Redirected to ', longURL);
-});
-
-// POST to /login in your Express server.
-app.post('/login', (req, res) => {
-  console.log('Hit login route');
-  const usr = req.body.username;
-  console.log('username is ', usr);
-  res.cookie('username', usr);
-  res.redirect('/urls');
-});
-
-//
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
   res.redirect('/urls');
 });
 
---
+++ helpers.js
@@ -0,0 +1,45 @@
+// Returns random six character alphanumeric string with varied case
+const generateRandomString = () => {
+  // assigns an array of random alphanumeric characters (lowercase)
+  let str = Math.random()
+    .toString(36)
+    .substring(2, 8)
+    .split('');
+
+  let alph = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < str.length; i++) {
+    // If it's a alphabet char, randomly uppercase
+    if (alph.indexOf(str[i]) > -1 && Math.round(Math.random()) === 1) {
+      str[i] = str[i].toUpperCase();
+    }
+  }
+
+  return str.join('');
+};
+
+// Returns true if email is taken, otherwise false
+const emailIsTaken = (email, usersObj) => {
+  for (let usr in usersObj) {
+    if (usersObj[usr].email === email.toLowerCase()) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Returns id for valid email & password, otherwise returns false
+const passwordIsValid = (email, usersObj, pw) => {
+  for (let usr in usersObj) {
+    if (
+      usersObj[usr].email === email.toLowerCase() &&
+      usersObj[usr].password === pw
+    ) {
+      console.log('Password is valid, usr is: ', usr);
+      return usr;
+    }
+  }
+  return undefined;
+};
+
+module.exports = { passwordIsValid, emailIsTaken, generateRandomString };
